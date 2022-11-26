321df1edb75ae84d842cee1420bfc59999053e14
Jean-Lionel <nijeanlionel@gmail.com>
2022-11-26 05:36:58 +0200

    add check tin method

--- OBR.php
+++ OBR.php
@@ -9,16 +9,29 @@ class OBR{
 	public $baseUrl = 'https://ebms.obr.gov.bi:9443/ebms_api/';
 
 	public function __construct(){
+		$token = $this->getToken();
 		$this->client = new GuzzleHttp\Client([
-			'headers' => [ 'Content-Type' => 'application/json' ]
+			'headers' => [ 
+				'Content-Type' => 'application/json',
+				'Authorization' => 'Bearer ' . $token
+			]
 		]);
 	}
 
-	public function postData($url, $value)
+	public function postData(string $url,array $value)
 	{
+		if(!$this->client){
+			$this->client = new GuzzleHttp\Client([
+				'headers' => [ 
+					'Content-Type' => 'application/json',
+					// 'Authorization' => 'Bearer ' . $token
+				]
+			]);
+
+		}
 		$response = $this->client->post($this->baseUrl. $url,[
-				"body" => json_encode($value)]);
-		return $response;
+			"body" => json_encode($value)]);
+		return json_decode($response->getBody()->getContents()) ;
 	}
 
 	public function getToken(){
@@ -27,7 +40,17 @@ class OBR{
 			'password' => '>1Q0Tn;a',
 		]);
 
-		return $response->getBody()->getContents();
+		return $response->result->token;
+	}
+
+	public function checkTin(string $tp_TIN){
+		$tp_TIN = trim($tp_TIN);
+		
+		$response =  $this->postData('checkTIN/',[
+			'tp_TIN' => $tp_TIN
+		]);
+
+		return $response;
 	}
 }
 
@@ -35,4 +58,4 @@ class OBR{
 
 $obrClient =  new OBR();
 
-print_r($obrClient->getToken());
+print_r($obrClient->checkTin('4002060640'));
